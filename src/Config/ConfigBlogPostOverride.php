59d0473f48a35905d1c6900dfd4a2c996b2dd535
Roberto Araya <roberto.araya@btester.cl>
2024-02-18 15:47:49 -0300

    Fix lint issue.

--- src/Config/ConfigBlogPostOverride.php
+++ src/Config/ConfigBlogPostOverride.php
@@ -3,8 +3,8 @@
 namespace Drupal\bt_blog\Config;
 
 use Drupal\Core\Cache\CacheableMetadata;
-use Drupal\Core\Config\ConfigFactoryOverrideInterface;
 use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
 use Drupal\Core\Config\StorageInterface;
 
 /**
